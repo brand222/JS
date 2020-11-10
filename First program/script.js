7aacb38ccbcf4faea40cd1a93b9aaf3754e97932
Brandon <brandon@hop.life>
2020-11-09 19:03:38 -0600

    First commit

---
+++ First program/script.js
@@ -0,0 +1,6 @@
+
+console.log("hello world!");
+let number = 2;
+if (number == 2) {
+    alert("the number is " + number);
+}
---
+++ Values & Variables/script.js
@@ -0,0 +1,37 @@
+let firstName = 'brandon';
+console.log(31000 + 3500 + 3500 + 3500 + 3500 + 3500);
+console.log(firstName);
+
+/* There are 7 datatypes in javascript*/
+//string
+let newName = "brandon";
+
+//number (double or integer)
+let newNumber = 1;
+
+//boolean
+let flag = false;
+
+//undefined (empty value)
+let nothing;
+
+//null (also means empty value)
+let anotherNothing = null;
+
+//Symbol (ES2015)
+
+//BigInt (Larger Integers than type number can hold)
+
+/* Javascript has 'dynamic typing' meaning that we do not have to explicitly
+define the datatype of the variable. Datatypes are determined automatically. */
+
+//logs number data type
+console.log(typeof 2.5);
+//logs string data type
+console.log(typeof 'hello');
+//logs boolean data type
+console.log(typeof true);
+//logs null data type
+console.log(typeof null);
+
+
